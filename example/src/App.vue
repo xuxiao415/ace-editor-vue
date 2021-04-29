27cc1f4f3ec2346cdb58ddeb6a79df72472194e2
wangzhengkai3 <wangzhengkai3@jd.com>
2021-04-29 17:49:23 +0800

    修改例子

--- example/src/App.vue
+++ example/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <h1>Ace editor for vue 2</h1>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
